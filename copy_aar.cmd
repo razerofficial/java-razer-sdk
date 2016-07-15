20213bc351dfefea87943698d539e9e4763cfe78
Tim Graupmann <tim@tagenigma.com>
2016-07-14 22:19:59 -0700

    Script updating the AAR library

---
+++ copy_aar.cmd
@@ -0,0 +1,7 @@
+ECHO ON
+COPY /Y store-sdk-standard-release.aar Samples\cc-sample\app\libs
+COPY /Y store-sdk-standard-release.aar Samples\controller-testapp\libs
+COPY /Y store-sdk-standard-release.aar Samples\game-sample\libs
+COPY /Y store-sdk-standard-release.aar Samples\iap-sample-app\libs
+COPY /Y store-sdk-standard-release.aar Samples\virtual-controller\app\libs
+PAUSE
